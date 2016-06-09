20d231d858f359da53117f394198a77faa75555c
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-06-09 17:29:02 +0200

    Replaced hardcoded mail and password with FrequentFlyer

--- src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
@@ -1,57 +1,58 @@
 package edu.iis.mto.bdd.cucumber.steps;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import edu.iis.mto.bdd.model.FrequentFlyerMember;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class UserAuthenticationSteps {
-	private WebDriver driver ;
-	
-	@Before
-	public void init(){
-		driver = new FirefoxDriver();
-	}
-	
+    private WebDriver driver;
+
+    @Before
+    public void init() {
+        driver = new FirefoxDriver();
+    }
+
     @Given("^(.*) is a registered Frequent Flyer$")
-    public void givenARegisteredFrequentFlyer(String userEmail) {}
+    public void givenARegisteredFrequentFlyer(FrequentFlyerMember frequentFlyerMember) {
+    }
 
     @When("^(.*) authenticates with a valid email address and password$")
-    public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(String user) {
-    	driver.get("http://localhost:8080/#/welcome");
-    	driver.findElement(By.name("email")).sendKeys("janina.kowalska@acme.com");
-		driver.findElement(By.name("password")).sendKeys("s3cr3t");
-		driver.findElement(By.name("signin")).click();
+    public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(FrequentFlyerMember frequentFlyerMember) {
+        driver.get("http://localhost:8080/#/welcome");
+        driver.findElement(By.name("email")).sendKeys(frequentFlyerMember.getEmail());
+        driver.findElement(By.name("password")).sendKeys(frequentFlyerMember.getPassword());
+        driver.findElement(By.name("signin")).click();
     }
 
     @Then("^(.*) should be given access to (?:her|his) account$")
-    public void thenTheUserShouldBeGivenAccessToAccount(String userName) {
-    	assertThat(driver.findElement(By.id("welcome-message")).getText(), equalTo("Witaj Janina"));    	
+    public void thenTheUserShouldBeGivenAccessToAccount(FrequentFlyerMember frequentFlyerMember) {
+        assertThat(driver.findElement(By.id("welcome-message")).getText(), equalTo("Witaj " + frequentFlyerMember.getFirstName()));
     }
 
     @Given("^(.*) has logged on$")
-    public void aUserHasLoggedOnAs(String user) {
-    	driver.get("http://localhost:8080/#/welcome");
-    	driver.findElement(By.name("email")).sendKeys("janina.kowalska@acme.com");
-		driver.findElement(By.name("password")).sendKeys("s3cr3t");
-		driver.findElement(By.name("signin")).click();
+    public void aUserHasLoggedOnAs(FrequentFlyerMember frequentFlyerMember) {
+        driver.get("http://localhost:8080/#/welcome");
+        driver.findElement(By.name("email")).sendKeys(frequentFlyerMember.getEmail());
+        driver.findElement(By.name("password")).sendKeys(frequentFlyerMember.getPassword());
+        driver.findElement(By.name("signin")).click();
     }
 
     @When("^(?:.*) views the home page$")
-    public void whenAUserViewsTheHomePage() {}
-    
+    public void whenAUserViewsTheHomePage() {
+    }
+
     @After
-    public void close(){
-    	driver.close();
+    public void close() {
+        driver.close();
     }
 
 }
