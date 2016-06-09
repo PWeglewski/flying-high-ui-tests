7e3f0b26d79d677080a01155fef4e3245eea5db0
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-06-09 18:11:03 +0200

    Implemented AuthenticationWorkflowSteps~

--- src/test/java/edu/iis/mto/bdd/cucumber/workflowsteps/AuthenticationWorkflowSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/workflowsteps/AuthenticationWorkflowSteps.java
@@ -1,7 +1,29 @@
 package edu.iis.mto.bdd.cucumber.workflowsteps;
 
+import edu.iis.mto.bdd.cucumber.pages.HomePage;
+import edu.iis.mto.bdd.cucumber.pages.LoginPage;
+import edu.iis.mto.bdd.model.FrequentFlyerMember;
+import net.thucydides.core.annotations.Step;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  * Created by piotr on 09.06.16.
  */
 public class AuthenticationWorkflowSteps {
+    LoginPage loginPage;
+    HomePage homePage;
+
+    @Step
+    public void enterEmailAndPassword(FrequentFlyerMember frequentFlyerMember) {
+        loginPage.open();
+        loginPage.signInWithCredentials(frequentFlyerMember.getEmail(), frequentFlyerMember.getPassword());
+    }
+
+    @Step
+    public void verifyWelcomeMessageFor(FrequentFlyerMember frequentFlyerMember) {
+        String welcomeMessage = homePage.getWelcomeMessage();
+        assertThat(welcomeMessage, equalTo("Witaj " + frequentFlyerMember.getFirstName()));
+    }
 }
