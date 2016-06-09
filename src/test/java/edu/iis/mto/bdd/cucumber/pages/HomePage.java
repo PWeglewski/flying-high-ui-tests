921c1b232a7d009ae2700387021f70079dff16fd
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-06-09 17:47:00 +0200

    HomePage implementation

--- src/test/java/edu/iis/mto/bdd/cucumber/pages/HomePage.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/pages/HomePage.java
@@ -1,7 +1,20 @@
 package edu.iis.mto.bdd.cucumber.pages;
 
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by piotr on 09.06.16.
  */
-public class HomePage {
+public class HomePage extends PageObject {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void selectMenuOptions(String... menuOptions) {
+        for (String menuChoice : menuOptions) {
+            $(".mainMenu").findBy(By.linkText(menuChoice)).click();
+        }
+    }
 }
